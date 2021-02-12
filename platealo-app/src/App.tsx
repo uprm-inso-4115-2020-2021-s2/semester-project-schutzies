caa38db306970fc07adae205554d7c77462fae60
keviinramiirez <kevin.ramirez3@upr.edu>
2021-02-12 19:49:53 -0400

    react with typescript setup

---
+++ platealo-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      Oh Welcomme Shutzies... What good?!
+    </div>
+  );
+}
+
+export default App;
