8a5b545c0a3a9ab9a909fddfd81750d96c9d7fae
keviinramiirez <kevin.ramirez3@upr.edu>
2021-02-15 15:25:20 -0400

    Routing to 4 components to start working on them

--- platealo-app/src/components/Navbar/Navlink.tsx
+++ platealo-app/src/components/Navbar/Navlink.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import './Navlink.css'
 
-const Navlink = () => {
-  return (
-    <div>
-
-    </div>
-  )
-}
+const Navlink = ({ children, path, onClick }: any) => (
+  <NavLink exact to={path}
+    // className={`
+    //   nav-link
+    //   text-hover${checkHoverText()}
+    //   nav-link${checkLinkText()}
+    // `}
+    onClick={onClick}
+  >
+    {children}
+  </NavLink>
+)
 
 export default Navlink
