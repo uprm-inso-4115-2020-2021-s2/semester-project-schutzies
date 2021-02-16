20297f219eaa98167b83fb799d39f9bc9a5492e3
keviinramiirez <kevin.ramirez3@upr.edu>
2021-02-16 13:52:44 -0400

    split view - right view logo centered

--- platealo-app/src/pages/Signup/Signup.tsx
+++ platealo-app/src/pages/Signup/Signup.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Signup.css'
+import PlatealoLogo from '../../assets/platealo_logo_transparent.png'
 
 const Signup = () => {
   return (
     <div className='signup'>
-      <h1>Signup component is Crackalaken ^_^</h1>
+      <div className='signup__left'>
+        SUP
+      </div>
+      <div className='signup__right'>
+        <div className='signup__logo'>
+          <img src={PlatealoLogo} width='200' height='200' alt='Platealo Logo' />
+        </div>
+      </div>
     </div>
   )
 }
