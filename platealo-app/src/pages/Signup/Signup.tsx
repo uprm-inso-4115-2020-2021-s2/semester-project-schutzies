03280f0b5ba28b7db2d73b522323c8cd81b1af5f
keviinramiirez <kevin.ramirez3@upr.edu>
2021-02-16 15:16:32 -0400

    finished right view - background image and logo

--- platealo-app/src/pages/Signup/Signup.tsx
+++ platealo-app/src/pages/Signup/Signup.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import './Signup.css'
 import PlatealoLogo from '../../assets/platealo_logo_transparent.png'
+import PlatealoBackgroundImg from '../../assets/platealo_background.jpg'
 
 const Signup = () => {
   return (
     <div className='signup'>
       <div className='signup__left'>
-        SUP
+        Signup Form
       </div>
       <div className='signup__right'>
-        <div className='signup__logo'>
-          <img src={PlatealoLogo} width='200' height='200' alt='Platealo Logo' />
-        </div>
+        <img className='signup__backImg' src={PlatealoBackgroundImg} />
+        <img className='signup__logo' src={PlatealoLogo} width='250' height='250' alt='Platealo Logo' />
       </div>
     </div>
   )
