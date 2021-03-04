4abd9dfd76095cae9c3020bb268ad6e30ae82293
keviinramiirez <kevin.ramirez3@upr.edu>
2021-03-04 15:36:49 -0400

    signin/signup for merging

--- platealo-app/src/pages/Signup/Signup.tsx
+++ platealo-app/src/pages/Signup/Signup.tsx
@@ -1,17 +1,88 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Signup.css'
 import PlatealoLogo from '../../assets/platealo_logo_transparent.png'
 import PlatealoBackgroundImg from '../../assets/platealo_background.jpg'
+import Navlink from '../../components/Navbar/Navlink'
 
 const Signup = () => {
+  const [username, setUsername] = useState('');
+  
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+  }
+
+  
+
+  /*
+  Kevins recommendation:
+  -Utilizar 4 usestate 
+  -4 funciones handle user/pass change etc cogen event param
+  - Event - user type, take that value and put in setState .target.value
+  -setState - changes the useState
+
+  
+  
+  */
   return (
     <div className='signup'>
       <div className='signup__left'>
-        Signup Form
+        <form className='form' onSubmit={handleSubmit}> 
+          <h1>Create your account</h1>
+          <div className='form-inputs'>
+            <label htmlFor='username' className = 'form-label'>
+                  <input 
+                  type = 'text'
+                  name='username'
+                  className = 'form-input'
+                  placeholder = ' Username'/>
+            </label>
+          </div>
+          <div className='form-inputs'>
+              <label htmlFor='email' className = 'form-label'>
+                    <input 
+                    type = 'email'
+                    name='email'
+                    className = 'form-input'
+                    placeholder = ' Email'/>
+              </label>
+          </div>
+          <div className='form-inputs'>
+              <label htmlFor='password' className = 'form-label'>
+                    <input 
+                    type = 'password'
+                    name='password'
+                    className = 'form-input'
+                    placeholder = ' Password'/>
+              </label>
+          </div>
+          <div className='form-inputs'>
+              <label htmlFor='confirm password' className = 'form-label'>
+                    <input 
+                    type = 'password'
+                    name='confirm password'
+                    className = 'form-input'
+                    placeholder = ' Confirm Password'/>
+              </label>
+          </div>
+          <div>
+            <button className="form-input-btn" type='submit' >
+              Sign up
+            </button>
+          </div>
+          <div>
+            <span className='form-input-login'>
+              Already have an account? <a href='Signin'>Sign In</a>
+            </span>
+          </div>
+          
+        </form>
       </div>
       <div className='signup__right'>
         <img className='signup__backImg' src={PlatealoBackgroundImg} />
-        <img className='signup__logo' src={PlatealoLogo} width='250' height='250' alt='Platealo Logo' />
+        
+        <Navlink path='/'>
+          <img className='signup__logo' src={PlatealoLogo} width='500' height='500' alt='Platealo Logo'  />
+        </Navlink>
       </div>
     </div>
   )
