c61bc7153084bdab7c6986ae73095be673b6b8c3
keviinramiirez <kevin.ramirez3@upr.edu>
2021-03-07 00:35:41 -0400

    implemented entire cards layout

--- platealo-app/src/pages/domain/ByIngredient/ByIngredient.tsx
+++ platealo-app/src/pages/domain/ByIngredient/ByIngredient.tsx
@@ -1,10 +1,58 @@
-import React from 'react'
+import React from 'react';
 import './ByIngredient.css'
+import { Search } from 'react-bootstrap-icons';
+import { Form, InputGroup, Col } from 'react-bootstrap'
+import Card from 'react-bootstrap/Card'
+import RecipePreview from '../../../components/RecipePreview/RecipePreview'
 
 const ByIngredient = () => {
   return (
     <div className='byIngredient'>
-      <h1>By Ingredient Component </h1>
+      <h2>BY INGREDIENT</h2>
+      <hr style={{ marginTop: '-6px' }} />
+
+      <Form.Row>
+        <Form.Group as={Col}>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>
+                {/* <FontAwesomeIcon icon="search" /> */}
+                <Search width='20' height='20' />
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            <Form.Control
+              type="text"
+              placeholder="Search Ingredient"
+            />
+          </InputGroup>
+        </Form.Group>
+      </Form.Row>
+
+      <div className='byIngredient__upperCards'>
+        <div className='byIngredient__leftCard'>
+          <Card>
+            LEFT CARD
+          </Card>
+        </div>
+
+        <div className='byIngredient__rightCard'>
+          <Card>
+            RIGHT CARD
+          </Card>
+        </div>
+      </div>
+
+      <div className='byIngredient__recipes pt-3'>
+        <RecipePreview />
+        <RecipePreview />
+        <RecipePreview />
+      </div>
+
+      <div className='byIngredient__recipes pt-3'>
+        <RecipePreview />
+        <RecipePreview />
+        <RecipePreview />
+      </div>
     </div>
   )
 }
