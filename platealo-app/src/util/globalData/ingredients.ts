bde29984f976b9bb6d417cd8feca77b8823ef43c
keviinramiirez <kevin.ramirez3@upr.edu>
2021-03-11 02:22:28 -0400

    hardcoded ingredients live here to use

--- platealo-app/src/util/globalData/ingredients.ts
+++ platealo-app/src/util/globalData/ingredients.ts
@@ -1,3 +1,37 @@
 import Ingredient from "../../models/Ingredient";
 
-// export default const ingredients =  
+export const ingredients = [
+  'Swiss Cheese',
+  'Meet Loaf',
+  'Butter',
+  'Strawberries',
+  'chocolate',
+  'Cheddar Cheese',
+  'Carrot',
+  'Salmon',
+  'Onion',
+  'Potatoes',
+  'Feta Cheese',
+  'Walnuts',
+  'Honey',
+  'Broccoli',
+  'Provolone Cheese',
+  'Steak',
+  'Mayonnaise',
+  'Parmesan Cheese',
+  'Feta Cheese',
+  'Sugar',
+  'Romaine lettuce',
+  'Mushrooms',
+  'Milk',
+]
+
+export const selectedIngredients = [
+  'Ground Beef',
+  'Italian Sauce',
+  'Eggs',
+  'Provolone Cheese',
+  'Oregano',
+  'Garlic',
+  'Brie Cheese',
+]
