94736a729e1c967989ccba04a710842e0f5ff017
keviinramiirez <kevin.ramirez3@upr.edu>
2021-03-15 20:39:31 -0400

    transfer ingredients per user interaction and search recipes ingredient ids (hardcoded)

--- platealo-app/src/pages/domain/ByIngredient/ByIngredient.tsx
+++ platealo-app/src/pages/domain/ByIngredient/ByIngredient.tsx
@@ -1,35 +1,130 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import './ByIngredient.css'
-import { Search, Plus, Dash } from 'react-bootstrap-icons';
-import { Card, Form, InputGroup, Col } from 'react-bootstrap'
+import { Search, Plus, Dash } from 'react-bootstrap-icons'
 import RecipePreview from '../../../components/RecipePreview/RecipePreview'
-import { Button } from 'react-bootstrap'
-import axios from 'axios';
+import axios from 'axios'
 import Ingredient from '../../../models/Ingredient'
+import Recipe from '../../../models/Recipe'
 // import { ingredients, selectedIngredients } from '../../../util/globalData/ingredients'
+import {
+  Card,
+  Form,
+  InputGroup,
+  Button,
+  Container,
+  Row,
+  Col
+} from 'react-bootstrap'
 
-// `localhost:8080/recipes?ids=603418bec9695c013853a67b,603424dd519bf90063346e76,603424e9519bf90063346e77,603424f5519bf90063346e78,60342674519bf90063346e79`
+
+// export default interface Recipe {
+//   id: number,
+//   name: string,
+//   recipeSteps: string,
+//   recipeDescription: string,
+//   ingredients: Ingredient[]
+//   updatedAt: number,
+//   credit: string,
+//   createdAt: number
+// }
+
+// export default interface Ingredient {
+//   id: string,
+//   name: string,
+//   updatedAt: number,
+//   createdAt: number
+// }
+
+// const recipess: Recipe[] = [
+//   new Recipe(1, "Cheese and Bacon Potato Rounds", "step1:Preheat oven to 400 degrees F (200 degrees C).step2:Brush both side of potato slices with butter; place them on an ungreased cookie sheet. Bake in the preheated 400 degrees F (200 degrees C) oven for 30 to 40 minutes or until lightly browned on both sides, turning once.step3:When potatoes are ready, top with bacon, cheese, and green onion; continue baking until the cheese has melted",
+//   "4 baking potatoes(cut into 1/2 inch slices), 1/4cup melted butter,8 slices bacon(coocked and crumbled),8 ounces shredded cheddar cheese,1/2 cup chopped green onions.",
+//   [
+//     new Ingredient("603418bec9695c013853a67b", "bacon", 1614026942815, 1614026942815),
+//     new Ingredient("603418bec9695c013853a67b", "bacon", 1614026942815, 1614026942815),
+//     new Ingredient("603418bec9695c013853a67b", "bacon", 1614026942815, 1614026942815),
+//     new Ingredient("603418bec9695c013853a67b", "bacon", 1614026942815, 1614026942815),
+//     new Ingredient("603418bec9695c013853a67b", "bacon", 1614026942815, 1614026942815)
+//   ],
+//   1614030498485,
+//   "allrecipes.com",
+//   1614030498485
+//   )
+// ]
+
+const recipess: string[] = [
+  'recipe 1',
+  'recipe 2',
+  'recipe 3',
+  'recipe 4',
+  'recipe 5',
+  'recipe 6',
+  'recipe 7',
+  'recipe 8',
+]
+
 const ByIngredient = () => {
-  const [ingredients, setIngredients] = useState([])
-  const [selectedIngredients, setSelectedIngredients] = useState([])
+  const [ingredients, setIngredients] = useState<Ingredient[]>([])
+  const [selectedIngredients, setSelectedIngredients] = useState<Ingredient[]>([])
+  const [recipes, setRecipes] = useState<Recipe[]>([])
 
   useEffect(() => {
-    axios.get(`/ingredients`)
-      .then((data: any) => {
-        console.log('ingredients', data['data'])
-        setIngredients(data['data'])
-      })
+    const fetchIngredients = async () => {
+      return new Promise((resolve, reject) =>
+        axios.get(`/ingredients`)
+          .then((data: any) => {
+            console.log('ingredients', data['data'])
+            setIngredients(data['data'])
+          })
+      )
+    }
+
+    fetchIngredients();
   }, [])
 
-  /** Pushes the given ingredient (selected by the user) into the selected ingredients array */
-  const handleIngredient = (ingredient: Ingredient) => {
-    // setSelectedIngredients(
-    //   [...selectedIngredients]
-    // )
+  useEffect(() => {
+    console.log(recipes)
+  }, [recipes])
+
+
+  /** Transfers the given ingredient from the 
+   *  ingredients list to the selected ingredient list. 
+   **/
+  const addIngredient = (ingredient: Ingredient) => {
+    // add the selected ingredient to ingredients list
+    setSelectedIngredients((prev: Ingredient[]) =>
+      [...prev, ingredient]
+    )
+
+    // remove the ingredient from selected list
+    const ingredientsClone = [...ingredients];
+    const selectedIndex = ingredients.findIndex(({ name }: any) => name === ingredient['name'])
+    ingredientsClone.splice(selectedIndex, 1)
+    setIngredients(ingredientsClone);
+  }
+
+  const removeIngredient = (ingredient: Ingredient) => {
+    // add the selected ingredient to selected list
+    setIngredients((prev: Ingredient[]) =>
+      [...prev, ingredient]
+    )
+
+    // remove the ingredient from selected list
+    const selectedIngredientsClone = [...selectedIngredients];
+    const selectedIndex = selectedIngredients.findIndex(({ name }: any) => name === ingredient['name'])
+    selectedIngredientsClone.splice(selectedIndex, 1)
+    setSelectedIngredients(selectedIngredientsClone);
   }
 
   const handleSearchRecipes = () => {
-    console.log(ingredients)
+    const fetchRecipes = async () => {
+      return new Promise((resolve, reject) =>
+        axios.get(`/recipes?ids=603418bec9695c013853a67b,603424dd519bf90063346e76,603424e9519bf90063346e77,603424f5519bf90063346e78,60342674519bf90063346e79`)
+          .then((data: any) => {
+            setRecipes(data['data'])
+            console.log('recipes', data['data'])
+          }))
+    }
+    fetchRecipes();
   }
 
 
@@ -59,7 +154,7 @@ const ByIngredient = () => {
           <Button
             key={i}
             className='byIngredient__chipButton d-flex align-items-center mr-3 mb-3'
-            onClick={() => handleIngredient(ingredient)}
+            onClick={() => addIngredient(ingredient)}
           >
             <Plus width='30' height='30' />
             <span>{ingredient['name']}</span>
@@ -67,48 +162,61 @@ const ByIngredient = () => {
         )}
       </div>
 
-      <Card className='byIngredient__selectedIngredients p-3'>
-        <div className='byIngredient__selectedButtons'>
-          <Button variant='secondary' className='byIngredient__chipButton d-flex align-items-center mr-5 mb-3' style={{ justifyItems: 'end' }}>
-            <Dash width='30' height='30' />
-            <span>SELECTED</span>
-          </Button>
-          <Button variant='secondary' className='byIngredient__chipButton d-flex align-items-center mr-5 mb-3'>
-            <Dash width='30' height='30' />
-            <span>SELECTED</span>
-          </Button>
-          <Button variant='secondary' className='byIngredient__chipButton d-flex align-items-center mr-5 mb-3'>
-            <Dash width='30' height='30' />
-            <span>SELECTED</span>
-          </Button>
-          <Button variant='secondary' className='byIngredient__chipButton d-flex align-items-center mr-5 mb-3'>
-            <Dash width='30' height='30' />
-            <span>SELECTED</span>
-          </Button>
-        </div>
+      {selectedIngredients.length === 0 ? null :
 
-        <Button
-          variant='secondary'
-          className='byIngredient__searchRecipes'
-          onClick={() => handleSearchRecipes()}
-        >
-          Search Recipes
-        </Button>
-      </Card>
+        <Card className='byIngredient__selectedIngredients p-3'>
+          <div className='byIngredient__selectedButtons'>
+            {selectedIngredients.map((ingredient: Ingredient, i: number) =>
+              <Button
+                key={i}
+                variant='secondary'
+                className='byIngredient__chipButton d-flex align-items-center mr-5 mb-3'
+                onClick={() => removeIngredient(ingredient)}
+              >
+                <Dash width='30' height='30' />
+                <span>{ingredient['name']}</span>
+              </Button>
+            )}
+          </div>
+
+          <Button
+            variant='secondary'
+            className='byIngredient__searchRecipes'
+            onClick={() => handleSearchRecipes()}
+          >
+            Search Recipes
+          </Button>
+        </Card>
+      }
 
 
       <h2 style={{ marginTop: '50px' }}>RECIPES</h2>
       <hr style={{ marginTop: '-6px' }} />
 
       <div className='byIngredient__recipes pt-3'>
-        <RecipePreview />
-        <RecipePreview />
-        <RecipePreview />
+        {recipes.map((recipe: Recipe, i: number) => {
+          return (
+            <>
+              <RecipePreview key={i} recipe={recipe} />
+              {(i % 3 === 0) ? <br /> : null}
+            </>
+          )
+        })}
       </div>
 
-      <div className='byIngredient__recipes pt-3'>
-        <RecipePreview />
-      </div>
+
+      {/* <div className='byIngredient__recipes pt-3'>
+        <RecipePreview key={2} recipe={'recipe 1'} />
+        <RecipePreview key={3} recipe={'recipe 1'} />
+        <br />
+        <RecipePreview key={4} recipe={'recipe 1'} />
+        <RecipePreview key={5} recipe={'recipe 1'} />
+        <RecipePreview key={6} recipe={'recipe 1'} />
+        <br />
+        <RecipePreview key={7} recipe={'recipe 1'} />
+        <RecipePreview key={8} recipe={'recipe 1'} />
+        <RecipePreview key={9} recipe={'recipe 1'} />
+      </div> */}
     </div>
   )
 }
